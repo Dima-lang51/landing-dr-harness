2567ca709f2faec3e96c657dd6118a9954f1157b
Dima-lang51 <68560471+Dima-lang51@users.noreply.github.com>
2021-11-01 03:58:50 +0300

    Finel code - Stage one

---
+++ app.js
@@ -0,0 +1,109 @@
+const buyBtns = document.querySelectorAll('.btn-product');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 200;
+
+if (buyBtns.length > 0) {
+  for (let index = 0; index < buyBtns.length; index++) {
+    const buyBtn = buyBtns[index];
+    buyBtn.addEventListener('click', function(e) {
+      const popupName = buyBtn.getAttribute('href').replace('#', '');
+      const curentPopup = document.getElementById(popupName);
+      popupOpen(curentPopup);
+      e.preventDefault();
+    });
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll('.popup-close');
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index];
+    el.addEventListener('click', function(e) {
+      popupClose(el.closest('.popup'));
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(curentPopup) {
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive){
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    curentPopup.classList.add('open');
+    curentPopup.addEventListener('click', function(e) {
+    if (!e.target.closest('.popup__content')) {
+      popupClose(e.target.closest('.popup'));
+    }
+  });
+}
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+      bodyUnLock();
+    }
+  }
+}
+
+
+function bodyLock() {
+  const lockPaddingValue = window.innerWidth - document.querySelector('.page').offsetWidth + 'px';
+
+if (lockPadding.length > 0) {
+  for (let index = 0; index < lockPadding.length; index++) {
+    const el = lockPadding[index];
+    el.style.paddingRight = lockPaddingValue;
+  }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function() {
+      unlock = true;
+    }, timeout);
+  }
+}
+
+function bodyUnLock() {
+  setTimeout(function() {
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index];
+      el.style.paddingRight = '0px';
+    }
+    body.style.paddingRight = '0px';
+    body.classList.remove('lock');
+  }, timeout);
+
+  unlock = false;
+  setTimeout(function() {
+    unlock = true;
+  }, timeout);
+}
+
+
+// Отображение второга блока каталога
+
+const casualProducts = document.querySelector('.casual');
+const eroticProducts = document.querySelector('.erotic');
+
+function showCasual() {
+  casualProducts.style.display = "flex";
+  eroticProducts.style.display = "none";
+  return true
+}
+function showErotic() {
+  casualProducts.style.display = "none";
+  eroticProducts.style.display = "flex";
+  return true
+}
+
